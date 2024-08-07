a6d59d6b86501e557e08032757f4db9311962697
Chandan-kr-dev <chandanramgar@gmail.com>
2024-08-07 22:51:56 +0530

    add preview btn

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -7,16 +7,16 @@ function Projects() {
       img: "../../../public/python1.png",
       title: "Calculator",
       linkTo: "https://github.com/Chandan-Kr-dev/calculator",
+      visit: "",
     },
     {
       img: "../../../public/Todoimg.jpg",
       title: "Todo App Using React",
       linkTo: "https://github.com/Chandan-Kr-dev/TodoReact",
+      visit: "https://main--todo-app-ck.netlify.app/",
     },
   ];
-  const gameProjects=[
-
-  ]
+  const gameProjects = [];
   useEffect(() => {
     setDetails(ProjectDetails);
   }, []);
@@ -27,41 +27,63 @@ function Projects() {
       <div className="text-center bg-blue-400 mt-10 rounded-lg p-10">
         <h1 className="text-white text-2xl mb-8">Web Devlopment Projects</h1>
         <div id="card" className="flex justify-evenly items-center flex-wrap">
-          
-            {ProjectDetails.map((det)=>{
-              return (
-                <div className="items-center justify-center ">
-
-                  <img className="h-36 w-36 rounded" src={det.img} alt="" />
-                  <div className="about text-center">
-                  <h1 className="text-2xl text-gray-600 font-semibold my-2">{det.title}</h1>
-                  <a className="bg-green-500 text-white  px-2 py-1 rounded-lg my-5" target="_blank" href={det.linkTo}>Code</a>
-
+          {ProjectDetails.map((det) => {
+            return (
+              <div className=" ">
+                <div className="about bg-slate-100 rounded-lg p-2 hover:">
+                  <img className="h-36 w-36 rounded-lg hover:opacity-50 items-center" src={det.img} alt="" />
+                  <div className=" ">
+                    <p>{det.title}</p>
                   </div>
                 </div>
-              )
-
-            })}
-          
-        </div>
-        {gameProjects.length>0 ? <div>
-
-        <h1 className="text-white text-2xl mb-8">Game Development Projects</h1>
-        <div id="card" className="flex justify-evenly items-center flex-wrap">
-          
-            {ProjectDetails.map((det)=>{
-              return (
-                <div>
-                  <img className="h-36 w-36 rounded" src={det.img} alt="" />
-                  <h1 className="text-2xl text-gray-600 font-semibold">{det.title}</h1>
-                  <a className="bg-green-500 text-white  px-2 py-1 rounded-lg my-2" target="_blank" href={det.linkTo}>Code</a>
+                <div className="buttons flex gap-5">
+                  <a
+                    className="bg-green-500 text-white  px-2 py-1 rounded-lg my-5"
+                    target="_blank"
+                    href={det.linkTo}
+                  >
+                    Code
+                  </a>
+                  <a
+                    href={det.visit}
+                    className="bg-green-500 text-white  px-2 py-1 rounded-lg my-5"
+                  >
+                    View
+                  </a>
                 </div>
-              )
-
-            })}
-          
+              </div>
+            );
+          })}
         </div>
-        </div>:null}
+        {gameProjects.length > 0 ? (
+          <div>
+            <h1 className="text-white text-2xl mb-8">
+              Game Development Projects
+            </h1>
+            <div
+              id="card"
+              className="flex justify-evenly items-center flex-wrap"
+            >
+              {ProjectDetails.map((det) => {
+                return (
+                  <div>
+                    <img className="h-36 w-36 rounded" src={det.img} alt="" />
+                    <h1 className="text-2xl text-gray-600 font-semibold">
+                      {det.title}
+                    </h1>
+                    <a
+                      className="bg-green-500 text-white  px-2 py-1 rounded-lg my-2"
+                      target="_blank"
+                      href={det.linkTo}
+                    >
+                      Code
+                    </a>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        ) : null}
       </div>
     </div>
   );
