bfb66f5f59216b502c1fd50069ac0cdba633a5fa
Chandan-kr-dev <chandanramgar@gmail.com>
2024-09-04 21:31:52 +0530

    more bugs solved

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,21 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: {
-        main: "./index.html",
-      },
-      output: {
-        entryFileNames: "index.js",
-        chunkFileNames: "index-234qer.js",
+export default defineConfig(() => {
+  const config = {
+    plugins: [react()],
+    base: "/",
+
+    build: {
+      rollupOptions: {
+        input: {
+          main: "./index.html",
+        },
+        output: {
+          entryFileNames: "[name].js",
+          chunkFileNames: "[name]-[hash].js",
+        },
       },
     },
-  },
+  };
 });
